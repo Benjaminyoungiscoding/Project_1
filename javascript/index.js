3fae9c7b89cb59cec3f7c2b308aea8793d1fa4f0
Benjamin Young <benjaminyoung73@gmail.com>
2025-10-30 22:05:01 -0700

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+function openLinks(){
+const nav = document.getElementById("nav");//get nav html element
+nav.classList.toggle("active")}// the nav element now becomes active class="nav active". the css nav.active kicks in.
